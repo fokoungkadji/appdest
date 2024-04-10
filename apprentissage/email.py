cbd3223c457bd41466e0a7e2aac9a40bcdbbcd13
fokoungkadji <kadjibezout@gmail.com>
2024-04-10 23:24:47 +0200

    initialisation d'un systeme dauthentification

--- apprentissage/email.py
+++ apprentissage/email.py
@@ -1,6 +1,6 @@
 EMAIL_USER_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'bezoutfokoung01@gmail.com'
-EMAIL_HOST_PASSWORD = 'paob fczc jxtc jomx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
