0a6a63a9db2d94fdd928255e7bc5c229fee3d2b7
fokoung bezout <kadjibezout@gmail.com>
2024-05-12 00:35:53 +0100

    envoie de mail reussi

--- apprentissage/email.py
+++
@@ -1,7 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kadjibezout@gmail.com'
-EMAIL_HOST_PASSWORD = 'zdhzpzgipbepocjd'
-EMAIL_PORT = 587
-
